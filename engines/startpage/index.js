8d47a225a3c5662a2effb19c1f89f349ee7ab140
fccview <comm.campione@gmail.com>
2026-03-11 14:09:26 +0000

    improve startpage search

--- engines/startpage/index.js
+++ engines/startpage/index.js
@@ -95,9 +95,16 @@ export default class StartpageEngine {
       headers: {
         "User-Agent": getRandomUserAgent(),
         Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
       },
+      redirect: "follow",
       method: "GET",
     });
     const html = await response.text();
